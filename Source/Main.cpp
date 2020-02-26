2ab6e00eec7acd84370853932b26c8e0622c0881
Connor Walsh <connorwalsh21@gmail.com>
2020-02-25 20:54:42 -0500

    First commit

---
+++ Source/Main.cpp
@@ -0,0 +1,138 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/GL.h>
+#include <cstdio>
+#include <vector>
+#include <glm/glm.hpp>
+
+// Globals
+float x_loc = 0.0f;
+float y_loc = 0.0f;
+float z_loc = 0.0f;
+
+float x_rot = 0.5f;
+float y_rot = -0.5f;
+float z_rot = 0.5f;
+
+
+void getInput()
+{
+	// Todo: Create moveable camera
+    const float camSpeed = 0.5f;
+}
+
+void renderTriangle()
+{
+    glRotatef(1.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+}
+
+void renderCube()
+{
+	glTranslatef(x_loc, y_loc, z_loc);
+	glBegin(GL_QUADS);
+
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+
+
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+
+
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+
+
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+
+
+	glVertex3f(-0.5f, 0.5f, 0.5f);
+	glVertex3f(0.5f, 0.5f, 0.5f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+
+	glVertex3f(-0.5f, -0.5f, 0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, 0.5f);
+
+
+	glEnd();
+
+
+	glRotatef(x_rot, 1.0f, 0.0f, 0.0f);
+	glRotatef(y_rot, 0.0f, 1.0f, 0.0f);
+	glRotatef(z_rot, 0.0f, 0.0f, 1.0f);
+}
+
+
+int main()
+{
+	int resX = 960;
+	int resY = 840;
+
+    GLFWwindow* window;
+
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    
+
+    window = glfwCreateWindow(resX, resY, "CW3D", NULL, NULL);
+    if (!window)
+    {
+        printf("There has been an error opening the window.");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        renderCube();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
