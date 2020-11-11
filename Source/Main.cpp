725e9b0dba91357c410e898bf9bc7829f6ea96d2
Connor Walsh <connorwalsh21@gmail.com>
2020-11-11 00:37:46 -0500

    Trying to remove muda stuff

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -122,18 +122,18 @@ int main()
 
     // World coordinate position
     glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(3.0f,  -2.4f, -3.2f),
-        glm::vec3(-4.3f,  8.4f, 2.3f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+        glm::vec3(0.0f, -1.0f, 0.0f),
+        glm::vec3(1.0f, -1.0f, 0.0f),
+        glm::vec3(2.0f, -1.0f, 0.0f),
+        glm::vec3(3.0f, -1.0f, 0.0f),
+        glm::vec3(0.0f, -1.0f, 1.0f),
+        glm::vec3(1.0f, -1.0f, 1.0f),
+        glm::vec3(2.0f, -1.0f, 1.0f),
+        glm::vec3(3.0f, -1.0f, 1.0f),
+        glm::vec3(0.0f, -1.0f, 2.0f),
+        glm::vec3(1.0f, -1.0f, 2.0f),
+        glm::vec3(2.0f, -1.0f, 2.0f),
+        glm::vec3(3.0f, -1.0f, 2.0f)
     };
 
     unsigned int VBO, VAO;
@@ -246,8 +246,8 @@ int main()
             // Make sure to initialize matrix to identity matrix first
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            //float angle = 20.0f * i;
+            //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
